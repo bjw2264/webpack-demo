f3eff89cb2d671605380309b1c3aaa4b602d55f9
鲍军伟 <ex-baojunwei001@pingan.com.cn>
2017-12-22 18:19:49 +0800

    初次提交

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from "lodash";
+import printMe from "./print";
+
+function component() {
+    var element = document.createElement('div');
+    var button = document.createElement('button');
+    element.innerHTML = _.join(['Hello', 'World', 'Webpack'], ' ');
+    button.innerHTML = 'click me';
+    button.onclick = printMe;
+    element.appendChild(button);
+
+    return element;
+}
+
+document.body.appendChild(component());
